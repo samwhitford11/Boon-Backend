4a4c1fa4bebaf77fc06f1a3182f2c3f48cdb40a5
samwhitford11 <samantha.fichte98@gmail.com>
2023-03-28 17:48:52 -0600

    adds middleware

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const DATABASE_URL = process.env.DATABASE_URL
 const PORT = process.env.PORT
-// create express app
+// create express app/ server
 const express = require("express");
 const app = express();
 
@@ -18,6 +18,7 @@ const app = express();
   app.use(cors()); 
   app.use(morgan("dev")); 
   app.use(express.json()); 
+  app.use(express.urlencoded({extended: true}))
   
 ///////////////////////////////
 // DATABASE CONNECTION
